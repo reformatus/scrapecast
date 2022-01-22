00bd510114768db75c168bd5c8ea8f4fb778e0b4
RedyAu <fodor.benedek2001@gmail.com>
2022-01-22 13:57:27 +0100

    test site

--- bin/crawlcast.dart
+++ bin/crawlcast.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
   print('Hello world!');
   print('This should still build.');
   print('Aand still should build.');
+  File html = File("docs/index.html");
+  List<String> lines = html.readAsLinesSync();
+  lines.removeLast();
+  html.writeAsString(lines.reduce((value, element) => value + "\n" + element));
 }
